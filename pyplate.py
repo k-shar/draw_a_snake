f17e9c1645a32080dfa27da5ec7277a9defd5e16
Kishan <kshar832@gmail.com>
2020-11-06 20:37:08 +0000

    Update pyplate.py

--- pyplate.py
+++ pyplate.py
@@ -1,41 +1,50 @@
 import pygame
 
+FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-WIDTH = 480
-HEIGHT = 360
-FPS = 60
 
 # Initialise Pygame
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Epic Py Game")
+
+# Load background image
+# BG = pygame.image.load("bg.jpg")
+# screen = pygame.display.set_mode(BG.get_rect().size)
+# BG.convert()
+# BG_blit = BG
+
+screen = pygame.display.set_mode([300, 400])
+
 clock = pygame.time.Clock()
 
 done = False
 while not done:
-    clock.tick(FPS)
-
-    screen.fill(WHITE)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         
-        loc = pygame.mouse.get_pos()
-        
         pressed = False
+        loc = pygame.mouse.get_pos()
         if pygame.mouse.get_pressed()[0]:
             print(f"Left Mouse Click at {loc}")
             pressed = True
 
         if pygame.key.get_pressed()[pygame.K_a]:
             print("A pressed")
-    
+        
+        if event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            # BG_blit = pygame.transform.scale(BG, screen.get_size())
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                done = True
         
         # Crosshair settings
         if pressed:
@@ -45,11 +54,15 @@ while not done:
             line = BLUE
             stroke = 1
 
+        # For BG image
+        # screen.blit(BG_blit, [0, 0])
+        screen.fill(WHITE)
+
         pygame.draw.line(screen, line, (0,0), (loc), stroke)
-        pygame.draw.line(screen, line, (WIDTH,0), (loc), stroke)
-        pygame.draw.line(screen, line, (0, HEIGHT), (loc), stroke)
-        pygame.draw.line(screen, line, (WIDTH, HEIGHT), (loc), stroke)
-        
+        pygame.draw.line(screen, line, (screen.get_rect()[2],0), (loc), stroke)
+        pygame.draw.line(screen, line, (0, screen.get_rect()[3]), (loc), stroke)
+        pygame.draw.line(screen, line, (screen.get_rect()[2], screen.get_rect()[3]), (loc), stroke)
+
+        clock.tick(FPS)
         pygame.display.update()
 pygame.quit()
-
