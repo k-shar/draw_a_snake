4c4bc04fb18753aa0ac034dbd55a41621ac6bedd
Kishan <kshar832@gmail.com>
2020-12-02 12:13:28 +0000

    Add files via upload

--- platformer/oldmain.py
+++ platformer/oldmain.py
@@ -8,148 +8,182 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+
 pygame.display.init()
-
-
 screen = pygame.display.set_mode((300, 300))
 pygame.display.set_caption("Platformer")
 
 
+# the Player and their carrot shooting carrot gun
 class Player:
-    def __init__(self, color):
-        self.Rect = pygame.Rect([50, 50], [20, 40])
-        self.x = self.Rect.x
-        self.y = self.Rect.y
+    def __init__(self, color, spawn_loc):
+        self.Rect = pygame.Rect([spawn_loc, [20, 40]])
         self.color = color
+        self.projectiles = []
+        self.landed = False
 
-        self.gravity = 1
-        self.speed = 5
+        self.speed = 4
+        self.jump_strength = -20
 
-        # to slow player down
-        self.drag = 0.6
+        self.bounce = -0.4
+        self.drag = 0.8
+        self.gravity = 1.5
 
-        # Velocity of player
+        self.x = spawn_loc[0]
+        self.y = spawn_loc[1]
+
         self.vx = 0
         self.vy = 0
 
-        self.landed = False
+    # To handle keypresses
+    def playerInput(self):
 
-
-    def draw(self, screen, platforms):
-
-        # Change in x and y
-        dx = 0
-        dy = self.gravity
-
-        # Process user input
         # Horizontal movement
-        if pygame.key.get_pressed()[pygame.K_a]:
-            dx -= self.speed
         if pygame.key.get_pressed()[pygame.K_d]:
-            dx += self.speed
+            self.dx += self.speed
+        if pygame.key.get_pressed()[pygame.K_a]:
+            self.dx -= self.speed
 
         # Vertical movement
-        if pygame.key.get_pressed()[pygame.K_w] and self.landed:
-            dy -= self.speed * 4
-            self.landed = False
         if pygame.key.get_pressed()[pygame.K_s]:
-            dy += self.speed
+            self.dy += self.speed
+        if pygame.key.get_pressed()[pygame.K_w] and self.landed:
+            self.vy = self.jump_strength
+            self.landed = False
 
-        # Scale back player y velocity
-        # as self.drag is less than 1
+    def rectifyCollisions(self):
+
+        # Change velocity by drag constants
+        self.dy += self.gravity
         self.vx *= self.drag
 
-        self.vx += dx
-        self.vy += dy
+        # add change in velocity t0 total velocitiy
+        self.vx += self.dx
+        self.vy += self.dy
 
-        # Test if collision is valid with move()
-        self.Rect = Player.move(self, self.vx, self.vy, platforms)
-        # Draw rect on screen
-        pygame.draw.rect(screen, self.color, self.Rect)
-
-
-    def move(self, dx, dy, platforms):
-        # Test for collisions with platforms
-
-        # handle movement on the X axis
-        self.x += dx
+        # Testing for horizontal collisions
+        self.x += self.vx
         self.Rect.x = round(self.x)
-        for platform in platforms:
+
+        for platform in self.platforms:
             if self.Rect.colliderect(platform.Rect):
-                if not self.hit:
-                    self.vx *= -1
-                self.hit = True
-                if dx > 0:
+
+                # if collided traveling right
+                if self.vx > 0:
                     self.Rect.right = platform.Rect.left
-                    self.x = self.Rect.x
-                if dx < 0:
+                # if collided traveling left
+                if self.vx < 0:
                     self.Rect.left = platform.Rect.right
-                    self.x = self.Rect.x
-            else:
-                self.hit = False
-        # handle movement on the Y axis
-        self.y += dy
+
+                self.x = self.Rect.x
+                self.vx *= self.bounce
+
+        # Testing for vertical collisions
+        self.y += self.vy
         self.Rect.y = round(self.y)
-        for platform in platforms:
+
+        for platform in self.platforms:
             if self.Rect.colliderect(platform.Rect):
-                if dy > 0:
+
+                # if collided traveling down
+                if self.vy > 0:
                     self.Rect.bottom = platform.Rect.top
                     self.landed = True
-                    self.y = self.Rect.y
-                if dy < 0:
+                # if collided traveling up
+                if self.vy < 0:
                     self.Rect.top = platform.Rect.bottom
-                    self.y = self.Rect.y
-                # Reset velocity when collision with floor or roof
-                self.vy = 0
 
-        # return correctly collided rect to draw()
-        return self.Rect
+                self.y = self.Rect.y
+                self.vy *= self.bounce
+
+    # Drawing the player rect on screen
+    def draw(self, screen, platfomrs):
+        self.screen = screen
+        self.platforms = platforms
+
+        # Change in velocity this frame
+        self.dx = 0
+        self.dy = 0
+        self.playerInput()
+        self.rectifyCollisions()
+
+        pygame.draw.rect(screen, self.color, self.Rect)
+
+    def recoil(self, dx, dy):
+        self.dx = dx * -10
+        self.dy = dy * -10
+        self.rectifyCollisions()
+        pygame.draw.rect(self.screen, self.color, self.Rect)
 
 
+# Carrot the player holds, used to shoot other carrots
 class Carrot:
-    def __init__(self, player, img):
-        self.player = player
-        self.img = pygame.image.load(img)
-        self.img.set_colorkey(WHITE)
+    def __init__(self, platforms):
+        self.platforms = platforms
+
+        self.img = pygame.image.load("carrot.png")
         self.img = pygame.transform.scale(self.img, (80, 30))
+        self.img.set_colorkey(WHITE)
+
         self.Rect = self.img.get_rect()
-        self.theta = 0
 
-        self.reflected = False
+        self.proj_list = []
 
-    def draw(self, screen, platforms):
-        # distances between mouse and carrot
-        self.xdistance = pygame.mouse.get_pos()[0] - self.Rect.centerx
-        self.ydistance = pygame.mouse.get_pos()[1] - self.Rect.centery
+    # draw the carrot onscreen
+    def draw(self, player):
+        self.drawCarrot(player)
 
-        # angle between mouse and carrot
-        if self.xdistance != 0:
-            self.theta = math.atan(-1 * self.ydistance / self.xdistance)
+    # Draw carrot the player holds
+    def draw(self, player):
+
+        # Distances between mouse and carrot
+        self.xdis = pygame.mouse.get_pos()[0] - self.Rect.centerx
+        self.ydis = pygame.mouse.get_pos()[1] - self.Rect.centery
+
+        # Calculate the angle between self and mouse pointer
+        if self.xdis != 0:
+            self.theta = math.atan(-1 * self.ydis / self.xdis)
             self.theta = math.degrees(self.theta)
-            # scale down theta between 0 and 180
-            self.theta = self.theta - (self.theta//180) * 180
-        else:
-            self.theta = 0
 
-        # correct for when theta needs to be above 180
-        # when mouse is below the carrot
+            # scale down theta between 0 and 180
+            # as arctan returns negative values
+            self.theta = self.theta - (self.theta//180) * 180
+
+        # Convert the angle to 0-360 opposed to 0-180
         if pygame.mouse.get_pos()[1] > self.Rect.centery:
             self.theta += 180
 
-        # rotate carrot by theta
+        # to handle edge cases
+        if self.theta == 0:
+            if self.Rect.centerx > pygame.mouse.get_pos()[0]:
+                self.theta = -180
+            if self.Rect.centerx < pygame.mouse.get_pos()[0]:
+                self.theta = 0
+
+            if self.Rect.centery > pygame.mouse.get_pos()[1]:
+                self.theta = 90
+            if self.Rect.centery < pygame.mouse.get_pos()[1]:
+                self.theta = -90
+
+        # Rotate img by theta
         self.rotated = pygame.transform.rotate(self.img, self.theta)
         self.Rect = self.rotated.get_rect(center=((player.x + player.Rect.w // 2, player.y + player.Rect.h // 3)))
 
+        # Blit rotated img
         screen.blit(self.rotated, (self.Rect.x, self.Rect.y))
 
-        print(self.theta)
-    def fireCarrot(self):            
+    def fireCarrot(self, proj_list):
+        dx = pygame.mouse.get_pos()[0] - self.Rect.centerx
+        dy = pygame.mouse.get_pos()[1] - self.Rect.centery
 
-        dx, dy = pygame.mouse.get_pos()[0] - self.Rect.centerx, pygame.mouse.get_pos()[1] - self.Rect.centery 
-        _len = math.hypot(dx, dy) 
-        dx, dy = dx/_len, dy/_len
-        carrotProjList.append(CarrotProj(screen, self.rotated, dx, dy, self.Rect.centerx, self.Rect.centery))
+        hypot = math.hypot(dx, dy)
+        dx /= hypot
+        dy /= hypot
 
+        proj_list.append(CarrotProj(screen, self.rotated, dx, dy, self.Rect.centerx, self.Rect.centery))
+        return dx, dy
+
+# Drawing projectiles shot by main carrot
 
 
 class CarrotProj:
@@ -157,70 +191,92 @@ class CarrotProj:
         self.screen = screen
         self.img = img
 
-        self.dx = dx
-        self.dy = dy
+        self.speed = 2
+        self.dx = dx * self.speed
+        self.dy = dy * self.speed
+        self.vx = 0
+        self.vy = 0
 
         # for float accuracy
         self.x = x
         self.y = y
+        self.pos = [self.x, self.y]
+
         # rect for collisions
         self.Rect = self.img.get_rect()
 
     def draw(self):
 
-        self.x += self.dx
-        self.y += self.dy
+        self.vx += self.dx
+        self.vy += self.dy
+
+        self.x += self.vx
+        self.y += self.vy
+        self.pos = [self.x, self.y]
+
         self.Rect.centerx = self.x
         self.Rect.centery = self.y
 
         screen.blit(self.img, (self.Rect.x, self.Rect.y))
 
 
-
+# Displaying platforms
 class Platform:
-    def __init__(self, Rect, color):
-        self.Rect = Rect
+    def __init__(self, color, rect):
+        self.Rect = rect
         self.color = color
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.Rect)
 
 
-player = Player(BLUE)
-carrot = Carrot(player, "carrot.png")
-carrotProjList = []
-
+# Create platforms
 platforms = []
-platforms.append(Platform(pygame.Rect(0, 280, 300, 20), RED))  # floor
-platforms.append(Platform(pygame.Rect(0, 0, 300, 20), RED))  # roof
-platforms.append(Platform(pygame.Rect(0, 0, 20, 300), RED))
-platforms.append(Platform(pygame.Rect(280, 0, 20, 300), RED))  # Walls
+platforms.append(Platform(RED, pygame.Rect(0, 0, 10, 300)))
+platforms.append(Platform(RED, pygame.Rect(0, 290, 300, 10)))
+platforms.append(Platform(RED, pygame.Rect(150, 200, 100, 10)))
 
-platform_rects = []
-for platform in platforms:
-    platform_rects.append(platform.Rect)
+# Create player
+player = Player(BLUE, [100, 150])
+carrot = Carrot(platforms)
+carrot_proj_list = []
 
 
 clock = pygame.time.Clock()
 done = False
 while not done:
-    # Every frame
+    screen.fill(BLACK)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+
+        # fire carrot on mousedown
         if event.type == pygame.MOUSEBUTTONDOWN:
-            Carrot.fireCarrot(carrot)
+            dx, dy = carrot.fireCarrot(carrot_proj_list)
+            player.recoil(dx, dy)
 
-    # Clear the screen
-    screen.fill(BLACK)
+    print(carrot_proj_list)
+    # Draw
     player.draw(screen, platforms)
-    carrot.draw(screen, platforms)
+    carrot.draw(player)
 
-    for carrotProj in carrotProjList:
-        carrotProj.draw()
-       # if carrotProj.Rect.collidelist(platform_rects) != -1:
-          #  carrotProjList.remove(carrotProj)
+    for carrot_proj in carrot_proj_list:
+        removed = False
+        carrot_proj.draw()
+
+        # delete carrot if not on screen
+        if not carrot_proj.Rect.colliderect(screen.get_rect()) and not removed:
+            carrot_proj_list.remove(carrot_proj)
+            removed = True
+
+        # delete if carrots center collides with platform
+        for platform in platforms:
+            if platform.Rect.collidepoint(carrot_proj.pos) and not removed:
+                carrot_proj_list.remove(carrot_proj)
+                removed = True
 
     for platform in platforms:
         platform.draw(screen)
