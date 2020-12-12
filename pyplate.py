58786853f25cf192b64a54492136f1a3f2cf40ed
Kishan <k.sharma8321@gmail.com>
2020-12-12 13:43:03 +0000

    Update pyplate.py

--- pyplate.py
+++ pyplate.py
@@ -34,39 +34,4 @@ def main():
 if __name__ == "__main__":
     main()
     pygame.quit()
-import pygame
-
-FPS = 60
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-SCREEN_SIZE = [300, 200]
-
-
-def main():
-    pygame.display.init()
-    screen = pygame.display.set_mode(SCREEN_SIZE, pygame.RESIZABLE)
-    pygame.display.set_caption("Pygame boilerplate")
-
-    clock = pygame.time.Clock()
-    done = False
-    while not done:
-
-        screen.fill(BLUE)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            if event.type == pygame.VIDEORESIZE:
-                screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-
-        pygame.display.update()
-        clock.tick(FPS)
-
-
-if __name__ == "__main__":
-    main()
-    pygame.quit()
+    
